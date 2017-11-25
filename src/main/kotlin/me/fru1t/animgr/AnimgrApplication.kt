9db8eb25cd1fb844a7ce23802bedda9947eb29db
kodlee yin <kodlee@fru1t.me>
2017-11-25 11:55:20 -0800

    Initial commit

---
+++ src/main/kotlin/me/fru1t/animgr/AnimgrApplication.kt
@@ -0,0 +1,12 @@
+package me.fru1t.animgr
+
+fun main(args: Array<String>) {
+  AnimgrApplication()
+}
+
+/**
+ * Entry point for the Animgr (Anime Manager) application. Loads necessary dependencies and
+ * triggers first window launching.
+ */
+class AnimgrApplication {
+}
